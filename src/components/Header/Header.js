ba76bc4751b76829322650562be3c510e734caaa
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-18 21:12:44 +0600

    login completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,19 +6,19 @@ const Header = () => {
     return (
         <div>
             <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-      <Nav.Link as={Link} to="/home">Home</Nav.Link>
-      <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-      </Nav>
-      <Nav className="ms-auto">
-      <Nav.Link as={Link} to="/login">Login</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                        </Nav>
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    if (error) {
+        return (
+          <div>
+            <p className='text-danger'>Error: {error.message}</p>
+          </div>
+        );
+      }
+
+      if(user){
+          navigate('/home');
+      }
+
     return (
         <div>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <button className='btn btn-rounded bg-primary text-light'>Sign In With Google</button>
+                    <button onClick={ () => signInWithGoogle()} className='btn btn-rounded bg-primary text-light'>Sign In With Google</button>
                 </Card.Body>
             </Card>
         </div>
