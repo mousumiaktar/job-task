937e55b4b6c3e4ee34ce08bf7b9b85815d8b76bf
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-19 00:35:49 +0600

    add task page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddTask from './components/AddTask/AddTask';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import ManageTask from './components/ManageTask/ManageTask';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/addtask" element={<AddTask></AddTask>}></Route>
+        <Route path="/manageTask" element={<ManageTask></ManageTask>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import User from '../User/User';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
+    useEffect( () => {
+        fetch('http://localhost:5000/user')
+        .then(res => res.json())
+        .then(data => setUsers(data));
+    },[])
+
+    const navigateUser= () =>{
+        navigate('/addtask')
+    }
+
     return (
         <div>
-            <h2>this is home</h2>
+            <h2>TeamUsers: {users.length}</h2>
+            <div className='row gy-5'>
+                {
+                    users.map(user => <User
+                    key={user._id}
+                    user={user}
+                    ></User>)
+                }
+            </div>
+            <button onClick={() => navigateUser()} className='btn btn-rounded bg-primary text-light mt-5'>Add Task</button>
         </div>
     );
 };
